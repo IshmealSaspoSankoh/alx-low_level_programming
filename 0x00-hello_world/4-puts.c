751edbc12e9f3a659a11a09676596a9a5b57626a
IshmealSaspoSankoh <ishmealsasposankoh@gmail.com>
2023-03-16 12:18:19 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry
- * Return : Always 0
+ *
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+-	return (0);
 }
