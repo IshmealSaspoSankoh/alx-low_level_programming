f60b5955d220ec7c69329e88fd1e1159c0492b85
IshmealSaspoSankoh <ishmealsasposankoh@gmail.com>
2023-03-21 05:33:15 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,16 @@
 /**
  * Print_alphabet - prints alphabet in lowercase
  *
- * Return: Always 0
+ * Return: Always 0 ( Success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char la;
 
 	for (la = 'a'; la <= 'z'; la++)
+	{
 		_putchar(la);
+	}
 
 	 _putchar('\n');
 }
